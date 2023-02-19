8292193cd2078632f5448f823ea9a9fdbb851361
Ben Braden <benjamin.braden@lions.nfl.net>
2023-02-19 18:36:58 -0500

    Set up

---
+++ foundation/airbnb/common/SearchQuery.py
@@ -0,0 +1,18 @@
+from typing import Tuple
+
+URL_ROOT = "https://www.airbnb.com/s/"
+
+
+class SearchQuery:
+    def __init__(self, city: str, state: str, country: str, **kwargs):
+        return
+
+    def get_query():
+        """
+        Function returns formatted url query to use for AirBnB listings based on class properties
+
+        Returns:
+            query (str): query url for listings in specified area and with specified parameters
+        """
+
+        return ""
